0b00822a71ae50acf7e87c60df51fa74942f7f88
arpitmishr <74408351+arpitmishr@users.noreply.github.com>
2025-07-01 12:27:47 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,13 @@
+// This is your list of blog posts.
+// When you create a new post in the admin panel, you must add its filename here.
+// The newest post should be at the top of the list if you want it to appear first.
+
+// Example:
+// const postFiles = [
+//   '2024-05-22-my-newest-post.md',
+//   '2024-05-21-my-first-post.md'
+// ];
+
+const postFiles = [
+
+];
